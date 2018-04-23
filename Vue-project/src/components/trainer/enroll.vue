b84c477e5fa6e4fd67c6c2cd3f3ca38e46b18e3a
zhangxin911 <453790557@qq.com>
2018-04-23 16:49:33 +0800

    $store.commit('方法',data)

--- Vue-project/src/components/trainer/enroll.vue
+++ Vue-project/src/components/trainer/enroll.vue
@@ -1,7 +1,7 @@
 <template>
   <el-container>
     <el-header class="t-center head-top">
-      <router-link to="/trainer"><i class="el-icon-arrow-left icon-back"></i></router-link>签到工具</el-header>
+      <router-link :to="{name:'trainer'}"><i class="el-icon-arrow-left icon-back"></i></router-link>签到工具</el-header>
     <el-main>
       <el-row>
         <img src="../../../build/logo.png">
@@ -15,7 +15,7 @@
         </el-form-item>
         <el-form-item label="签到时间">
           <el-col :span="11">
-            <el-date-picker type="fixed-time" placeholder="开始时间" v-model="form.date1" style="width: 100%;"></el-date-picker>
+            <el-time-picker type="fixed-time" placeholder="开始时间" v-model="form.date1" style="width: 100%;"></el-time-picker>
           </el-col>
           <el-col class="line" :span="2">-</el-col>
           <el-col :span="11">
@@ -30,7 +30,7 @@
 
 
       <el-row :gutter="10" class="mt1">
-        <el-button><router-link to="/trainer">取消发布</router-link></el-button>
+        <el-button><router-link v-bind:to="{name:'trainer'}">取消发布</router-link></el-button>
         <el-button type="primary">确认发布</el-button>
       </el-row>
     </el-main>
--- Vue-project/src/components/trainer/start-class.vue
+++ Vue-project/src/components/trainer/start-class.vue
@@ -15,11 +15,11 @@
          </el-form-item>
         <el-form-item label="时间跨度">
           <el-col :span="11">
-            <el-date-picker type="fixed-time" placeholder="选择时间" v-model="form.date1" style="width: 100%;"></el-date-picker>
+            <el-time-picker placeholder="选择时间" v-model="form.date1" style="width: 100%;"></el-time-picker>
           </el-col>
           <el-col class="line" :span="2">-</el-col>
           <el-col :span="11">
-            <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+            <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
           </el-col>
         </el-form-item>
 
